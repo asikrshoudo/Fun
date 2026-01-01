47e20e5b3a75331c029a72f6eb381d776c030276
asikrshoudo <shoudoashikurshek@gmail.com>
2026-01-02 05:35:33 +0600

    Initial commit

---
+++ fun.py
@@ -0,0 +1,15 @@
+hungry = True
+coding = False
+
+def eat():
+    print("Eating 🍔")
+
+def sleep():
+    print("Sleeping 😴")
+
+if hungry:
+    eat()
+elif coding:
+    print("Coding 💻")
+else:
+    sleep()
